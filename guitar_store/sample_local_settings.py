5266b17b9eacea99f025829ac04edb076f646b23
Nick <nxriso@hotmail.com>
2019-03-03 19:33:24 +0200

    initial commit

---
+++ guitar_store/sample_local_settings.py
@@ -0,0 +1,8 @@
+SECRET_KEY = ''
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
---
+++ guitar_store/settings.py
@@ -0,0 +1,57 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'guitar_store.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'guitar_store.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'guitar_store',
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'Europe/Athens'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+from .local_settings import *  # noqa
