7becd3ffd583e239c656af8be1cc841ff38c91ad
Nick <nxriso@hotmail.com>
2019-03-05 19:06:56 +0200

    implement Guitar backend

--- guitars/admin.py
+++ guitars/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from .models import Brand
+from .models import Brand, Guitar
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug': ('name',),
+    }
+
+
+@admin.register(Guitar)
+class GuitarAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug': ('model_name',),
     }
--- guitars/models.py
+++ guitars/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from easy_thumbnails.fields import ThumbnailerImageField
+
 
 class Brand(models.Model):
     name = models.CharField(max_length=255)
@@ -9,3 +11,30 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Guitar(models.Model):
+    ACOUSTIC = 'Acoustic'
+    ELECTRO_ACOUSTIC = 'Electro-Acoustic'
+    ELECTRIC = 'Electric'
+
+    GUITAR_TYPE_CHOICES = (
+        (ACOUSTIC, 'Acoustic'),
+        (ELECTRO_ACOUSTIC, 'Electro-Acoustic'),
+        (ELECTRIC, 'Electric'),
+    )
+
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+    model_name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    guitar_type = models.CharField(
+        max_length=255,
+        choices=GUITAR_TYPE_CHOICES,
+        default=ACOUSTIC,
+    )
+
+    djent = models.BooleanField(default=False)
+    image = ThumbnailerImageField('upload_to=images/guitar_model/')
+
+    def __str__(self):
+        return '%s %s' % (self.brand, self.model_name)
